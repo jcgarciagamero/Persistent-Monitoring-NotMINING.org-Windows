eef47882ec576fb93f5f0b8bc86520d797065a7d
José C. García Gamero <jcgarciagamero@gmail.com>
2018-05-08 18:47:58 +0200

    Add files via upload

---
+++ download.py
@@ -0,0 +1,9 @@
+import wget 
+url = 'https://notmining.org/monitoring/netstat.py'
+filename = wget.download(url)
+url = 'https://notmining.org/monitoring/persistentMonitoring.py'
+filename = wget.download(url)
+url = 'https://notmining.org/monitoring/persistentMonitoring.vbs'
+filename = wget.download(url)
+url = 'https://notmining.org/monitoring/upgrade.py'
+filename = wget.download(url)
