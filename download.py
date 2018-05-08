e65f2bf17246b9b557d6514e92113fd425fd70b6
José C. García Gamero <jcgarciagamero@gmail.com>
2018-05-08 18:46:59 +0200

    Delete download.py

--- download.py
+++
@@ -1,9 +0,0 @@
-import wget 
-url = 'https://notmining.org/monitoring/netstat.py'
-filename = wget.download(url)
-url = 'https://notmining.org/monitoring/persistentMonitoring.py'
-filename = wget.download(url)
-url = 'https://notmining.org/monitoring/persistentMonitoring.vbs'
-filename = wget.download(url)
-url = 'https://notmining.org/monitoring/upgrade.py'
-filename = wget.download(url)
