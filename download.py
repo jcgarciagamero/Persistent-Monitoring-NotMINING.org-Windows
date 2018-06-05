a31baae2d384863178170929dc7b185398fd6efe
José C. García Gamero <jcgarciagamero@gmail.com>
2018-06-05 16:01:19 +0200

    Update download.py

--- download.py
+++ download.py
@@ -7,3 +7,4 @@ url = 'https://notmining.org/monitoring/persistentMonitoring.vbs'
 filename = wget.download(url)
 url = 'https://notmining.org/monitoring/upgrade.py'
 filename = wget.download(url)
+url = 'https://notmining.org/monitoring/logo.png'
